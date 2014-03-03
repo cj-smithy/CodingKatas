e108b3ac18827f3bd0ca0a7583b8db4827ae5dc3
cj-smithy <chris.smith@red-gate.com>
2014-03-03 17:38:56 +0000

    Uncle Bobs first two tests.

---
+++ BowlingKata/BowlingGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingKata
+{
+    public class BowlingGame
+    {
+        int _score = 0;
+
+        public void roll(int pinsKnockedDown)
+        {
+            _score += pinsKnockedDown;
+        }
+
+        public int GetScore()
+        {
+            return _score;
+        }
+    }
+}
--- BowlingKata/Tests/BowlingTests.cs
+++ BowlingKata/Tests/BowlingTests.cs
@@ -11,9 +11,34 @@ namespace BowlingKata.Tests
     class BowlingTests
     {
         [TestCase]
-        public void GameStarts()
+        public void TestGutterGameScoresZero()
         {
-
+            var game = BuildGame();
+            RollMany(game, 20, 0); 
+            Assert.AreEqual(0, game.GetScore());
         }
+
+        [TestCase]
+        public void TestAllOnesGame()
+        {
+            var game = BuildGame();
+            RollMany(game, 20, 1);
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        #region Helper stuff
+        private static BowlingGame BuildGame()
+        {
+            return new BowlingGame();
+        }
+
+        private static void RollMany(BowlingGame game, int rolls, int pinsKnockedDown)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.roll(pinsKnockedDown);
+            }
+        }
+        #endregion Helper stuff
     }
 }
