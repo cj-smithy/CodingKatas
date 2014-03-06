8af93729e845e227fbd5feac6d93620c1cdf4272
cj-smithy <chris.smith@red-gate.com>
2014-03-06 17:38:02 +0000

    Fixing Game class.

--- BowlingKata/Game.cs
+++ BowlingKata/Game.cs
@@ -22,6 +22,7 @@ namespace BowlingKata
             }
         }
 
+
         public void Roll(int pinsKnockedDown)
         {
             if (_frames[_currentFrame].HasEnded)
@@ -35,30 +36,8 @@ namespace BowlingKata
             }
 
             _frames[_currentFrame].Roll(pinsKnockedDown);
-<<<<<<< HEAD
-
-            if (_currentFrame >= 10)
-            {
-                if (_frames[_currentFrame].IsStrike())
-                {
-                    // is this the 10th frame
-                    if (_currentFrame == 10)
-                    {
-                        _frames.Add(new Frame());
-                    }
-                    else if (_currentFrame == 11)
-                    {
-                        _frames.Add(new Frame(1));
-                    }
-                }
-                else if (_frames[_currentFrame].IsSpare() && _currentFrame == 11)
-                {
-                    _frames.Add(new Frame(1));
-                }
-            }
-=======
->>>>>>> 9c8155f3194a90a5bd836edc44d7f23c072a9867
         }
+
 
         public int GetScore()
         {
@@ -72,11 +51,7 @@ namespace BowlingKata
                 }
                 else if (frame.IsSpare())
                 {
-<<<<<<< HEAD
-                    score += 10 + (frameIndex < _frames.Count ? SpareBonus(frameIndex) : 0);
-=======
                     score += 10 + SpareBonus(frameIndex);
->>>>>>> 9c8155f3194a90a5bd836edc44d7f23c072a9867
                 }
                 else
                 {
